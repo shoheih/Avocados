179f40a39cf51ef1b4f84084c65fc30b529fc4f1
shoheih <shoheigithub@gmail.com>
2020-06-06 20:03:44 +0900

    present a new view using SwiftUI

--- Avocados/View/RecipeCardView.swift
+++ Avocados/View/RecipeCardView.swift
@@ -12,6 +12,8 @@ struct RecipeCardView: View {
     // MARK: - PROPERTIES
     
     var recipe: Recipe
+    var hapticImpact = UIImpactFeedbackGenerator(style: .heavy)
+    @State private var showModal: Bool = false
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
@@ -63,6 +65,13 @@ struct RecipeCardView: View {
         .background(Color.white)
         .cornerRadius(12)
         .shadow(color: Color("ColorBlackTransparentLight"), radius: 8, x: 0, y: 0)
+        .onTapGesture {
+            self.hapticImpact.impactOccurred()
+            self.showModal = true
+        }
+        .sheet(isPresented: self.$showModal) {
+            RecipeDetailView(recipe: self.recipe)
+        }
     }
 }
 
